c8b6992f3e3969aa2b7066428ba97cc19687ba40
Laura733 <lali733@gmail.com>
2021-05-23 23:53:57 +0200

    Adding JS fetch and paint result

--- src/js/01_header.js
+++ src/js/01_header.js
@@ -1,13 +1,11 @@
 "use strict";
 
-const searchButton = document.querySelector(".js-search");
-const textSerch = document.querySelector(".js-textSearch");
-const listResult = document.querySelector(".js-listResult");
-const listFavorites = document.querySelector(".js-listFavorites");
-let imageDefault = "https://via.placeholder.com/210x295/ffffff/666666/?
-text=TV";
+const searchButton = document.querySelector(".js-buttonSearch");
+const textSearch = document.querySelector(".js-textSearch");
+const listResultElement = document.querySelector(".js-listResult");
+const listFavoritesElement = document.querySelector(".js-listFavorites");
+const imageDefault =
+  "https://via.placeholder.com/210x295/ffffff/666666/?text=TV";
 
 let listFavorites = [];
 let listResult = [];
-
-
--- src/js/02_main.js
+++ src/js/02_main.js
@@ -1,10 +1,37 @@
 "use strict";
 
-function handlersearchButton() {
-  const changetextSearch = textSerch.value;
-  fetch(`http://api.tvmaze.com/search/shows?${changetextSearch}`)
+function handlerSearchButton() {
+  const userTextSearch = textSearch.value;
+  fetch(`https://api.tvmaze.com/search/shows?q=${userTextSearch}`)
     .then((response) => response.json())
-    .then((data) => {});
+    .then((data) => {
+      listResult = data;
+
+      paintseries();
+    });
 }
 
-searchButton.addEventListener("click", handlersearchButton);
+function paintseries() {
+  for (const serie of listResult) {
+    const listSerie = document.createElement("li");
+    const titleSerie = document.createElement("h3");
+    listSerie.appendChild(titleSerie);
+    const nameSerie = document.createTextNode(serie.show.name);
+    titleSerie.appendChild(nameSerie);
+    listResultElement.appendChild(listSerie);
+    const serieImage = document.createElement("img");
+    listSerie.appendChild(serieImage);
+
+    if (serie.show.image === null) {
+      serieImage.src = imageDefault;
+    } else {
+      serieImage.src = serie.show.image.medium;
+    }
+  }
+}
+
+searchButton.addEventListener("click", handlerSearchButton);
+
+/* Escuchar evento sobre la serie para añadir a favoritos
+pintar en lista de favoritos
+evento sobre botón de borrar lista de favoritos*/
